f3fa21a623b44e568bc4f234057b0fca49c9a8ca
George <speedportmp@yahoo.comjp>
2025-01-01 13:43:58 +0900

    2回目コミット

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      aaaa
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
