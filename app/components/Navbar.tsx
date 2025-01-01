3d821a33325148e43918612cc51ab0d423f21aeb
George <speedportmp@yahoo.comjp>
2025-01-01 22:38:10 +0900

    MyTodos追加＆ページ遷移整備

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 text-white py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold">George's Portfolio</Link>
+        <Link href="/" className="text-2xl font-bold">George&apos;s Portfolio</Link>
         <ul className="flex space-x-4">
           <li>
             <Link href="/#about" className="hover:text-gray-300">About</Link>
