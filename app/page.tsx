d01ee96379877be8705b8e0bfc3078cbeacf2ce4
George <speedportmp@yahoo.comjp>
2025-01-05 16:11:22 +0900

    db接続

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,20 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
+import { neon } from '@neondatabase/serverless';
+
 export default function Home() {
+
+  async function create(formData: FormData) {
+    'use server';
+    // Connect to the Neon database
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get('comment');
+    // Insert the comment from the form into the Postgres database
+    await sql('INSERT INTO comments (comment) VALUES ($1)', [comment]);
+  }
+
+
   return (
     <div>
       <Navbar />
@@ -12,6 +25,10 @@ export default function Home() {
         <Projects />
         <Contact />
       </main>
+      <form action={create}>
+        <input type="text" placeholder="write a comment" name="comment" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
